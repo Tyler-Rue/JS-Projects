f88ea99d094378f735f9b5ff52fefca0bf3a9ab3
Tyler-Rue <Tyler.Anthony.Rue@gmail.com>
2022-06-17 16:14:44 -0400

    rearranged file contents

--- basic_javascript_projects/project2_functions/JS/main.js
+++ basic_javascript_projects/project2_functions/JS/main.js
@@ -4,3 +4,9 @@ function my_first_function() {
     var str3 = str1 + " " + str2;          //Adding str1 and str2 together with a space in between            
     document.getElementById("Button_Text").innerHTML = str3;      //.innerHTML modifies an existing HTML element
 }
+
+function my_function() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";         //+= is an alternate way to add variables together
+    document.getElementById("Concat").innerHTML = sentence;
+}
---
+++ basic_javascript_projects/project3_math_operators/JS/main.js
@@ -0,0 +1,4 @@
+function add () {
+    var add = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + add;
+}
